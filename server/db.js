c0acb9698fc028cca9223c625e7594447a0d8453
yaelawrence18 <yaelawrence18@gmail.com>
2026-05-12 11:30:34 +0100

    fix: db connexion via env variables

--- server/db.js
+++ server/db.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2")
 
 const connection = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "bandzaye"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306
 })
 
 connection.getConnection((err) => {
